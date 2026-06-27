d3ed4c860ff5dd84ead769489ea5ed55ac83aa36
Vladimir Romecin <vromecin-ext@wondrhealth.com>
2026-06-26 20:31:32 -0400

    Add environment configuration and LangSmith setup

---
+++ app/utils/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_API_KEY"] = LANGCHAIN_API_KEY
+os.environ["LANGCHAIN_PROJECT"] = "FinTech_Compliance_Project"
